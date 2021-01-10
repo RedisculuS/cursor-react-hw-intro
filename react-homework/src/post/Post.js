00115273974bcb05ddc1306ec806e027f9c5044f
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2021-01-10 21:11:10 +0200

    fixes after verifying

--- react-homework/src/post/Post.js
+++ react-homework/src/post/Post.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import '../post/Post-style.css';
 
-const Post = (props) => {
+const Post = ({author, date, content, image}) => (
 
-    return(
-        <div className="post">
-            <div className="anakyn-Post">
-                <div className="info">
-                    <img className="pic" src={props.author.photo}></img>
-                    <div className="author-Info">
-                        <p className="name">{props.author.name}</p>
-                        <p className="nickname">{props.author.nickname}</p>
-                        <p className="data">{props.date}</p>
-                    </div>
-                    <p className="content">{props.content}</p>
-                    <img className="post-Image" src={props.image}></img>
+    <div className="post">
+        <div className="anakyn-Post">
+            <div className="info">
+                <img className="pic" src={author.photo}></img>
+                <div className="author-Info">
+                    <p className="name">{author.name}</p>
+                    <p className="nickname">{author.nickname}</p>
+                    <p className="data">{date}</p>
                 </div>
+                <p className="content">{content}</p>
+                <img className="post-Image" src={image}></img>
             </div>
         </div>
-    )
-}
+    </div>
+
+);
 
 export default Post;
